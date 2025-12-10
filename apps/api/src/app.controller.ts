663156513194c8e1eba136b609fd3c75861b6baf
horetsky <nadvorniyoleh@icloud.com>
2025-12-10 10:15:00 +0200

    Add API service to docker-compose. Clean up unused dependencies.

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
 import { ConfigService } from "@/config.service";
+import { Public } from "@/decorators";
 
 @Controller()
 export class AppController {
     constructor(private readonly configService: ConfigService) {}
 
     // Health Check
+    @Public()
     @Get()
     getHello(): string {
         return `Hello World ${this.configService.env("PORT")}!`;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
 
+    app.setGlobalPrefix("/api");
+
     app.use(cookieParser());
 
     app.enableCors(corsConfig);
